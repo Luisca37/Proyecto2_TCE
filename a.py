1ddd453e941cda1a69745ec15e94081f417da10c
Mercurio1509 <moisesleiva01@gmail.com>
2023-10-17 13:41:21 -0600

    Update a.py

--- a.py
+++ a.py
@@ -6,13 +6,13 @@ from scipy.signal import lfilter
 from rcosdesign import rcosdesign
 
 
-def nrz_polar_encoding(data):
+def nrz_encoding(data):
     encoded_signal = []
     for bit in data:
         if bit == 1:
-            encoded_signal.extend([1, -1])  # 1 se representa con una transición de +1 a -1
+            encoded_signal.extend([1] * 2)  # 1 se representa con una señal constante alta
         else:
-            encoded_signal.extend([-1, 1])  # 0 se representa con una transición de -1 a +1
+            encoded_signal.extend([-1] * 2)  # 0 se representa con una señal constante baja
     return encoded_signal
 
 def plot_nrz_polar(data, encoded_data):
@@ -28,11 +28,79 @@ def plot_nrz_polar(data, encoded_data):
 
 
 
+Ns=16
 
+
+def rz_polar_encoding(data):
+    encoded_signal = []
+    for bit in data:
+        if bit == 1:
+            encoded_signal.extend([1, 0, -1])  # 1 se representa con +1, seguido de 0, seguido de -1
+        else:
+            encoded_signal.extend([-1, 0, 1])  # 0 se representa con -1, seguido de 0, seguido de +1
+    return encoded_signal
+
+def plot_rz_polar(data, encoded_data):
+    plt.figure(figsize=(10, 2))
+    plt.plot(data, drawstyle='steps-post', label='Señal original')
+    plt.plot(encoded_data, drawstyle='steps-post', label='RZ Polar')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Amplitud')
+    plt.title('Codificación RZ Polar')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
 # Ejemplo de uso
 data = [1, 0, 1, 1, 0, 0, 1]
-encoded_data = nrz_polar_encoding(data) # a[k]
+encoded_data = rz_polar_encoding(data)
+plot_rz_polar(data, encoded_data)
+
+
+
+def manchester_encoding(data):
+    encoded_signal = []
+    for bit in data:
+        if bit == 1:
+            encoded_signal.extend([1, -1])  # 1 se representa con una transición de +1 a -1
+        else:
+            encoded_signal.extend([-1, 1])  # 0 se representa con una transición de -1 a +1
+    return encoded_signal
+
+def plot_manchester(data, encoded_data):
+    plt.figure(figsize=(10, 2))
+    plt.plot(data, drawstyle='steps-post', label='Señal original')
+    plt.plot(encoded_data, drawstyle='steps-post', label='Manchester')
+    plt.xlabel('Tiempo')
+    plt.ylabel('Amplitud')
+    plt.title('Codificación Manchester')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+# Ejemplo de uso
+#data = [1, 0, 1, 1, 0, 0, 1]
+encoded_data = manchester_encoding(data)
+plot_manchester(data, encoded_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Ejemplo de uso
+#data = [1, 0, 1, 1, 0, 0, 1]
+#data = (np.random.rand(Ns) > 0.5).astype(int)
+encoded_data = nrz_encoding(data) # a[k]
 plot_nrz_polar(data, encoded_data)
 
 
